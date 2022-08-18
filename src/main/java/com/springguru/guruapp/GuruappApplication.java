9c4a3fcb48c23d6b9866771e9a287cf657ce6bad
Intasir Niloy <intasirniloy@gmail.com>
2022-08-18 22:28:27 +0600

    Initial commit

---
+++ src/main/java/com/springguru/guruapp/GuruappApplication.java
@@ -0,0 +1,13 @@
+package com.springguru.guruapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuruappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuruappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springguru/guruapp/GuruappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springguru.guruapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuruappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
